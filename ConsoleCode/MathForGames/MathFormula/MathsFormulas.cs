583baa9714dcae6d716ebcc353e480a78da7b726
Jonathan <jarzate26.ja10@gmail.com>
2022-09-21 15:34:35 -0700

    AddedMathForGames

---
+++ ConsoleCode/MathForGames/MathFormula/MathsFormulas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIE
+{
+    internal class MathsFormulas
+    {
+        public static float BasicQuadraic(float x)
+        {
+            return x * x + 2 * x - 7;
+        }
+    }
+}
---
+++ ConsoleCode/MathForGames/MathFormula/Program.cs
@@ -0,0 +1,10 @@
+﻿using AIE;
+class program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("hello");
+        MathsFormulas.BasicQuadraic(5.0f);
+        return;
+    }
+}
