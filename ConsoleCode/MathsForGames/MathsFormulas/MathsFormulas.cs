77f7e83986ace66510497dbf80180dc2cdc1f4a3
Jonathan <jarzate26.ja10@gmail.com>
2022-09-21 16:53:59 -0700

    Updated MathsFormulas

---
+++ ConsoleCode/MathsForGames/MathsFormulas/MathsFormulas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIE
+{
+  public class MathsFormulas
+    {
+        public static float BasicQuadratic(float x)
+        {
+            return x * x + 2 * x - 7;
+        }
+        public struct QuadraticRoots
+        {
+            public float rootA;
+            public float rootB;
+            public bool HasRoot;
+        }
+        public static QuadraticRoots GetQuadraticRoots(float a, float b , float c)
+        {
+            float numA = -b + MathF.Sqrt(b * b - 4 * a * c);
+            float numb = -b + MathF.Sqrt(b * b - 4 * a * c);
+            float den = 2 * a;
+            QuadraticRoots roots = new QuadraticRoots();
+            roots.rootA = numA;
+            roots.rootB = numb;
+            if((b*b-4*a*c)<0)
+            {
+                roots.HasRoot = false;
+            }
+            else
+            {
+                roots.HasRoot = true;
+            }
+            return roots;
+
+        }
+    }
+}
---
+++ ConsoleCode/MathsForGames/MathsFormulas/Program.cs
@@ -0,0 +1,15 @@
+﻿using AIE;
+
+class Program
+{
+     static void main(string[] args)
+    {
+      float quad = MathsFormulas.BasicQuadratic(5.0f);
+        
+        float sqrtofFour=(float)Math.Sqrt(4);
+
+        MathsFormulas.QuadraticRoots roots = MathsFormulas.GetQuadraticRoots(1, -5, 4);
+        
+        return;
+    }
+}
