1b5b0447902fea11852e446c8aee1bdbe1c9a43f
Jonathan <jarzate26.ja10@gmail.com>
2022-09-28 16:38:36 -0700

    a

---
+++ ConsoleCode/MathsForGames/MathLibraryTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+} 
