f496f0d937f40f722f2104414dc3f896e62289bc
Jonathan <jarzate26.ja10@gmail.com>
2022-09-29 16:15:23 -0700

    fixed vector and math library

---
+++ ConsoleCode/MathsForGames/MathLibrary/Colour.cs
@@ -0,0 +1,40 @@
+﻿namespace MathLibrary
+{
+    public struct Colour
+    {
+        public UInt32 colour;
+
+        public byte Red
+        {
+            get { return (byte)((colour >> 24) & 0xff); }
+            set { colour = (colour & 0x00ffffff) | ((UInt32)value << 24); }
+        }
+
+        public byte Green
+        {
+            get { return (byte)((colour >> 16) & 0xff); }
+            set { colour = (colour & 0xff00ffff) | ((UInt32)value << 16); }
+        }
+
+        public byte Blue
+        {
+            get { return (byte)((colour >> 8) & 0xff); }
+            set { colour = (colour & 0xffff00ff) | ((UInt32)value << 8); }
+        }
+
+        public byte Alpha
+        {
+            get { return (byte)((colour >> 0) & 0xff); }
+            set { colour = (colour & 0xffffff00) | ((UInt32)value << 0); }
+        }
+
+        public Colour(byte r, byte g, byte b, byte a)
+        {
+            colour = 0;
+            Red = r;
+            Green = g;
+            Blue = b;
+            Alpha = a;
+        }
+    }
+}
---
+++ ConsoleCode/MathsForGames/MathLibrary/MathFormulas.cs
@@ -0,0 +1,61 @@
+﻿namespace AIE
+{
+    public static class MathFormulas
+    {
+        //Problem A
+        public static float BasicQuadratic(float x)
+        {
+            return x * x + 2 * x - 7;
+        }
+
+        //Problem B
+        public struct QuadraticRoots
+        {
+            public float rootA;
+            public float rootB;
+            public bool hasRoots;
+        }
+
+        public static QuadraticRoots GetQuadraticRoots(float a, float b, float c)
+        {
+            float numA = -b + MathF.Sqrt(b * b - 4 * a * c);
+            float numB = -b - MathF.Sqrt(b * b - 4 * a * c);
+
+            float den = 2 * a;
+
+            QuadraticRoots roots = new QuadraticRoots();
+            roots.rootA = numA / den;
+            roots.rootB = numB / den;
+
+            if ((b * b - 4 * a * c) < 0)
+            {
+                roots.hasRoots = false;
+            }
+            else
+            {
+                roots.hasRoots = true;
+            }
+
+            return roots;
+        }
+
+        //Problem C
+
+        public static float LinearEquation(float s, float e, float t)
+        {
+            float result = s + t * (e - s);
+            return result;
+        }
+
+        //Problem D
+
+        public static float Distance(float x1, float y1, float x2, float y2)
+        {
+            return MathF.Sqrt((x2 - x1) * (x2 - x1) * (y2 - y1) * (y2 - y1));
+        }
+
+        //Problem E
+
+
+    }
+}
