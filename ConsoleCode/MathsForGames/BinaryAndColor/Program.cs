82851cb76725cb2b5728f5306b84b41523899859
Jonathan <jarzate26.ja10@gmail.com>
2022-09-29 11:48:56 -0700

    fixedTrigAndAddedBinary

---
+++ ConsoleCode/MathsForGames/BinaryAndColor/Program.cs
@@ -0,0 +1,30 @@
+﻿using Raylib_cs;
+
+public class Program
+{
+    public static int Main()
+    {
+        // Initializing - LOAD THE THINGS
+        const int screenW = 800;
+        const int screenH = 450;
+
+        Raylib.InitWindow(screenW, screenH, "Raylib");
+        Raylib.SetTargetFPS(60);
+
+        // Game Loop - PLAY THE GAME
+        while (!Raylib.WindowShouldClose())
+        {
+            // Update
+
+            // Draw
+            Raylib.BeginDrawing();
+
+            Raylib.EndDrawing();
+        }
+
+        // Deinitializing - UNLOAD THE THINGS
+        Raylib.CloseWindow();
+
+        return 0;
+    }
+}
