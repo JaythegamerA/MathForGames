a057a400fc41b538aef01fd9dd72f334105e0ca7
Jonathan <93285753+JaythegamerA@users.noreply.github.com>
2022-11-17 09:40:33 -0800

    RedoingTank

--- ConsoleCode/MathsForGames/GraphicalTestApplication/BulleSpawner.cs
+++ ConsoleCode/MathsForGames/GraphicalTestApplication/BulleSpawner.cs
@@ -6,19 +6,6 @@ namespace TankProject
 {
     public static class BulletSpawner
     {
-        public static Bullet SpawnBullet(string pathToBulletSprite)
-        {
-            Bullet newBullet = new Bullet();
-
-            //The program throws an error if it can't find the bullet sprite
-            if (!File.Exists(pathToBulletSprite))
-            {
-                throw new Exception("File not found at path: " + pathToBulletSprite);
-            }
-
-            newBullet.sprite = Raylib.LoadTexture(pathToBulletSprite);
-
-            return newBullet;
-        }
+        
     }
 }
